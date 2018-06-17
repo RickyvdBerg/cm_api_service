bb2f9890cf560f97220873c114d21e9067c7e454
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2018-06-17 17:37:11 +0200

    update

--- flows/payment_controller.js
+++ flows/payment_controller.js
@@ -1,5 +1,6 @@
 const getPaymentToken = require('../actions/payment_getToken');
 const payment = require('../actions/payment');
+var sendSms = require('../actions/send_sms');
 
 
 module.exports = {
@@ -9,10 +10,20 @@ module.exports = {
         getPaymentToken.getPaymentToken(json.client_id, json.client_secret).then((accessToken) => {
             if (accessToken) {
                 payment.getPayment (json.amount, json.merchant_reference, accessToken).then((body) => {
-                    if (!false) {
-                        res.status(200);
-                        res.send({
-                            "URL": body.uri
+                    if (body) {
+                        sendSms.sendSMS(json.token, "De payment link is: " + body.uri, json.phoneNumber, "CM").then((bool) => {
+                            if (bool){
+                                res.status(200);
+                                res.send({
+                                    "Message": "SMS send out"
+                                });
+                            }
+                            else {
+                                res.status(409);
+                                res.send({
+                                    "Message": "Something went wrong"
+                                });
+                            }
                         });
                     }
                 });
